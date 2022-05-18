55293d7ca371983b7f8daef87626b9b3a1e5b17a
Leonardo S Santos <leonardo.s.santos@infinitysys.com.br>
2022-05-18 20:27:40 -0300

    verificador de numero primo desenvolvido

--- op-basicas-numeros.js
+++ op-basicas-numeros.js
@@ -7,10 +7,28 @@ function ePar(n){
 }
 
 function eParT(n){
-    return (n % 2 == 0)? true : false;
+return (n % 2 == 0)? true : false;
 }
 
+function temRestoZero(a,b){
+    return (a % b == 0)? true : false;
+}
 
+    
+function ePrimo(n){
+    let nrDivPerf = 0;
+    let denominador = n;
 
-let n = eParT(5);
-console.log (n);
+    while (denominador > 0 ){
+        if (temRestoZero(n,denominador)){
+            nrDivPerf++;
+        }
+        denominador--;
+    }
+    if (nrDivPerf == 2){
+        return true;
+    }else{
+        return false;
+    }
+}
+
