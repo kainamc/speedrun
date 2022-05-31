728cd7b56eac4e0ff57ce537d7b6140be3cf9904
kainamc <kaina_cardoso@hotmail.com>
2022-05-31 20:04:03 -0300

    commit caracol

---
+++ caracol.js
@@ -0,0 +1,9 @@
+let args = process.argv.slice(2)
+
+let parede = a = Number(args[0]);
+let deslc = b  =  Number(args[1]);
+let escor = c =  Number(args[2]);
+let d = b+c;
+let x = a/d;
+
+console.log(x);
