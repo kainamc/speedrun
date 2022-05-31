73032e130fc5d577ab46f7a3ceaa5999505cb48b
kainamc <kaina_cardoso@hotmail.com>
2022-05-30 21:17:57 -0300

    commit op-bas-num. 2

--- op-basicas-numeros.js
+++ op-basicas-numeros.js
@@ -1,14 +1,14 @@
-function ePar(n){
-    if (n % 2 == 0){
-        return true;
-    }else{
-        return false;
-    }
-}
+// function ePar(n){
+//     if (n % 2 == 0){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
 
-function eParT(n){
-return (n % 2 == 0)? true : false;
-}
+// function eParT(n){
+// return (n % 2 == 0)? true : false;
+// }
 
 function temRestoZero(a,b){
     return (a % b == 0)? true : false;
@@ -30,35 +30,41 @@ function ePrimo(n){
     }else{
         return false;
     }
-}
-
-function ePrimoT(n){
-    let nrDivPerf = 0;
-    let denominador = n;
-
-    while (denominador > 0 ){
-        if (temRestoZero(n,denominador)){
-            nrDivPerf++;
-        }
-        denominador--;
-    }
-    return (nrDivPerf == 2)? true : false;
     
 }
-
-
-
-function multiplicar(a,b){
-    let resultado = 0;
-    let i = b;
-
-
-    while (i>0){
-        resultado += a;
-        i--
+// console.log(ePrimo(3));
+console.log('números de 1 a 100 dos quais são primos');
+    for (i = 0; i<= 100; i++){
+        console.log( i + ' ' + ePrimo(i));
     }
-    console.log(resultado);
-}
 
-let mult = multiplicar(6,3);
+// function ePrimoT(n){
+//     let nrDivPerf = 0;
+//     let denominador = n;
+
+//     while (denominador > 0 ){
+//         if (temRestoZero(n,denominador)){
+//             nrDivPerf++;
+//         }
+//         denominador--;
+//     }
+//     return (nrDivPerf == 2)? true : false;
+    
+// }
+// console.log(nrDivPerf)
+
+
+// function multiplicar(a,b){
+//     let resultado = 0;
+//     let i = b;
+
+
+//     while (i>0){
+//         resultado += a;
+//         i--
+//     }
+//     console.log(resultado);
+// }
+
+// let mult = multiplicar(6,3);
 
