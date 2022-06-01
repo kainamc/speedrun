5e926fd5918d08d6c06676c091e03012f8721dcc
kainamc <kaina_cardoso@hotmail.com>
2022-06-01 20:17:21 -0300

    caracol 1st commit

--- caracol.js
+++ caracol.js
@@ -1,9 +1,42 @@
-let args = process.argv.slice(2)
+// the snail Garry is at the bottom of a tower and needs to get out of it, help him!
+// tap three arguments of your choice:
+// 1 - lenght of the tower
+// 2 - how many meter garry can move before he takes a breath
+// 3 - how many meters he slips while breathing
 
-let parede = a = Number(args[0]);
-let deslc = b  =  Number(args[1]);
-let escor = c =  Number(args[2]);
-let d = b+c;
-let x = a/d;
 
-console.log(x);
+
+var args = process.argv.slice(2)
+
+var tower = a = Number(args[0]);
+var up = b  =  Number(args[1]);
+var down = c =  Number(args[2]);
+var d = (b-c);
+var count = a;
+
+console.log("Garry precisa escalar esta torre de " + a + "m\n" )
+console.log(count);
+
+while(count > 0){
+count-=b;
+console.log("Garry moveu-se " + b + "m");
+// console.log("Ele está a " + parseInt(count-b) + "m do topo da torre.");
+console.log(count);
+
+if(count <= 0){
+    console.log("chegou");
+}else{
+    count += c;
+    console.log("Garry escorregou " + c + "m" );
+    // console.log("Ele está a " + (count+c) + "m do topo da torre.");
+    console.log(count);
+    }
+}
+
+
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log("fim")
